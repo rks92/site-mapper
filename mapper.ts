c877261cb3c56e3fcefccb2cf92c77716af91543
Rahul Shardha <rahul.shardha@gmail.com>
2022-03-17 22:29:37 -0400

    Added sleep

--- mapper.ts
+++ mapper.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { msleep } from 'sleep';
 
 class SiteNode {
   constructor(url: string, parent: SiteNode = null) { this.url = url; }
@@ -10,39 +11,40 @@ class SiteNode {
   staticURLs: [string];
 }
 
-const urlStack = new Array<SiteNode>();
-// urlStack.push(process.argv[2]);
-urlStack.push(new SiteNode(process.argv[2]));
-visit(new SiteNode(process.argv[2]));
-
-let awaitingPromises: [Promise<string>];
+const awaitingPromises = new Array<Promise<string | void>>();
 const visitedSites = new Map<string, SiteNode>();
+
+const urlStack = new Array<SiteNode>();
+urlStack.push(new SiteNode(process.argv[2]));
 
 async function visit(site: SiteNode) {
   // Make request
   // Add to promises list
-  return axios
-    .get(site.url)
-    .then(res => {
-      // Populate the site node values
-      // Add children
-      // Add values to the stack if they haven't been visited
-      // Add node to visited sites
-      const regex = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g
-      const links = res.data.match(regex);
+  awaitingPromises.push(
+    axios
+      .get(site.url)
+      .then(res => {
+        // Populate the site node values
+        // Add children
+        // Add values to the stack if they haven't been visited
+        // Add node to visited sites
+        const regex = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g
+        const links = res.data.match(regex);
 
-      for (const link of links) {
-        const url = new URL(link);
-        console.log(url);
-      }
+        for (const link of links) {
+          const url = new URL(link);
+          console.log(url);
+        }
 
-      console.log(links);
-      // console.log(res);
-    });
+        console.log(links);
+        // console.log(res);
+      })
+    );
 }
 
-while(urlStack.length != 0) {
+while(urlStack.length !== 0) {
   visit(urlStack.pop());
+  msleep(500);
 }
 
 Promise.allSettled(awaitingPromises).then((settledPromises) => {
